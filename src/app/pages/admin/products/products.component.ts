53d01e504b7c85ec05f606c6d789635162a91830
S. M. Rafi <smrafialam@gmail.com>
2024-07-31 01:39:47 +0600

    updates odule

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
+import { CategoryService } from '../../../services/category/category.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
+
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  isSidePanelVisible: boolean = false;
+  productObj:any = {
+    "productId": 0,
+    "productSku": "",
+    "productName": "",
+    "productPrice": 0,
+    "productShortName": "",
+    "productDescription": "",
+    "createdDate": new Date(),
+    "deliveryTimeSpan": "",
+    "categoryId": 0,
+    "productImageUrl": "",
+  }
+  categoryList:any[] = [];
+  
+  constructor(private productService: ProductService, private categoryService:CategoryService){
 
+  }
+
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories(){
+    this.categoryService.getAllCategories().subscribe((res:any)=>{
+      console.log(res);
+      this.categoryList = res.data;
+    })
+  }
+
+  openSidePanel(){
+    this.isSidePanelVisible = true;
+  }
+
+  closeSidePanel(){
+    this.isSidePanelVisible = false;
+  }
 }
